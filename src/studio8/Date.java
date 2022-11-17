6e6860ee8a8eab562a1d8614a5907278a6c30bbf
sabrinaelbouhali <sabrinaelbouhali@sabrinas-air.dhcp.wustl.edu>
2022-11-17 14:15:10 -0600

    studio 8

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,6 +1,25 @@
 package studio8;
 
 public class Date {
+	private int month;
+	private int day;
+	private int year;
+	private boolean holiday;
+	
+public Date(int month, int day, int year, boolean holiday) {
+	this.month=month;
+	this.day=day;
+	this.year=year;
+	this.holiday=holiday;
+	
+}
+public String toString() {
+	if(holiday==false) {
+	return month+ "/" + day + "/" + year;}
+	else {
+		return month+ "/" + day + "/" + year + "";}
+	}
+
 
 
     public static void main(String[] args) {
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -2,8 +2,32 @@ package studio8;
 
 
 public class Time {
+	private int hour;
+	private int minute;
+	private  boolean is12;
+	
+public Time(int hour, int minute, boolean is12) {
+	this.hour=hour;
+	this.minute=minute;
+	this.is12=is12;
+}
+public String toString() {
+	if(is12==false) {
+		return hour+ ":" + minute;}
+	else  {
+		if(hour>=12) {
+			return (hour-12)+ ":" + minute + " PM";}
+		else {
+			return hour+ ":" + minute + "AM";}
+		}	
+	}
+	
 
 	public static void main(String[] args) {
+		//creating a new object based off of class 
+		Time result= new Time(15,23,true);
+		System.out.println(result.toString());
+		
  
     	
     }
